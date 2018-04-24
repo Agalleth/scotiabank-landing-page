3d0b1d0c3bffaa7a2c551ce448a16a4ed94e6799
AdrixSC <adriana.be@outlook.com>
2018-04-24 04:36:41 -0500

    agregando nuevo componente de banner

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,10 +1,21 @@
 import React, {Component} from "react";
+//import Logo from './src/scotiabank-logo.png';
 
 class Banner extends Component {
     render() {
         return (
-            <div className="row banner">
-                <h1>INSERTE BANNER Y BORRE ESTO</h1>
+            <div>
+                <nav className="navbar navbar-default">
+                    <a className="navbar-brand" href="#"><img className="logo" src='' alt="logo"/></a>
+                    <a className="btn" href="#">SOLICITA TU TARJETA</a>
+                </nav>
+                <div className="row banner">
+                    <div className="col-md-5 text-banner">
+                        <h2 className="colo-red">Empieza a disfrutar la vida como quieras.</h2>
+                        <p>En Scotiabank tenemos la Tarjeta de Crédito ideal para ti, cada una diseñada
+                            para adaptarse a tu estilo de vida.</p>
+                    </div>
+                </div>
             </div>
         )
     }

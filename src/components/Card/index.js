a25835ca7e7654e39a278ae06c7ebdee146d98ee
AdrixSC <adriana.be@outlook.com>
2018-04-23 22:47:38 -0500

    agregando componentes y jalando base de datos

---
+++ src/components/Card/index.js
@@ -0,0 +1,14 @@
+import React, {PureComponent} from 'react';
+
+class Card extends PureComponent {
+
+    render() {
+        //const {array} = this.props; //estado para el componente de las tarjetas
+        //console.log(array)
+        return (
+            <div>Card</div>
+        )
+    }
+}
+
+export default Card;

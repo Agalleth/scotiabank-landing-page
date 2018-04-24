5546df24da5059477c609dbac0eb616731c21e51
AdrixSC <adriana.be@outlook.com>
2018-04-24 01:13:51 -0500

    subiendo estado del select

--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -1,11 +1,48 @@
-import React, {Component} from 'react';
+import React, {PureComponent} from 'react';
+import { ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
-class Select extends Component {
-    render () {
-        return (
-            <div>Select</div>
-        )
+
+class Select extends PureComponent  {
+    constructor(props) {
+      super(props);
+  
+      this.toggle = this.toggle.bind(this);
+      this.state = {
+        dropdownOpen: false,
+        value: ''
+      };
     }
-}
+  
+    toggle() {
+      this.setState({
+        dropdownOpen: !this.state.dropdownOpen
+      });
+    }
 
-export default Select
+    handleOnchange = (item) => {
+        console.log(item)
+    }
+
+    select(event) {
+        console.log(event.target.id)
+        
+      }
+  
+    render() {
+        const {array} = this.props;
+      return (
+        <ButtonDropdown className="backgroundButton" isOpen={this.state.dropdownOpen} toggle={this.toggle} onChange={this.handleOnchange} >
+          <DropdownToggle caret>
+          ¿Cuál es tu ingreso?
+          </DropdownToggle>
+          <DropdownMenu>
+            {
+                array.map((item) => <DropdownItem key={item.id} id={item.amount} onClick={this.select}>{item.amount}</DropdownItem>) 
+            }
+          </DropdownMenu>
+        </ButtonDropdown>
+      );
+    }
+  }
+
+export default Select;
